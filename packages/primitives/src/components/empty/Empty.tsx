5b4475bb1a6374ab27591477b7a0890dce1bcc92
장호빈 <devhobin@gmail.com>
2023-02-21 17:40:27 +0900

    eslint 설정을 추가하였다

--- packages/primitives/src/components/empty/Empty.tsx
+++ packages/primitives/src/components/empty/Empty.tsx
@@ -2,6 +2,11 @@ import { useEffect, useState } from "react";
 
 export const Empty = () => {
   const [a, setA] = useState<boolean>(false);
-  useEffect(() => {}, []);
+  useEffect(() => {
+    console.log(setA);
+    if (a) {
+      console.log("ho");
+    }
+  }, []);
   return null;
 };
