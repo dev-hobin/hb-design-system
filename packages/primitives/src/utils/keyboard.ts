00a4178bc05b57eabc6bc6da46f39ccb20afd014
장호빈 <devhobin@gmail.com>
2023-02-23 04:46:46 +0900

    keyboard keys 유틸을 추가하였다

---
+++ packages/primitives/src/utils/keyboard.ts
@@ -0,0 +1,19 @@
+export enum Keys {
+  Space = " ",
+  Enter = "Enter",
+  Escape = "Escape",
+  Backspace = "Backspace",
+  Delete = "Delete",
+
+  ArrowLeft = "ArrowLeft",
+  ArrowUp = "ArrowUp",
+  ArrowRight = "ArrowRight",
+  ArrowDown = "ArrowDown",
+
+  Home = "Home",
+  End = "End",
+  PageUp = "PageUp",
+  PageDown = "PageDown",
+
+  Tab = "Tab",
+}
