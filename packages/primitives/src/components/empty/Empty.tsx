bd4602ac86c00fb811c16b23a4701f69b047abe2
장호빈 <devhobin@gmail.com>
2023-02-22 21:04:35 +0900

    Empty 컴포넌트를 삭제하였다

--- packages/primitives/src/components/empty/Empty.tsx
+++
@@ -1,6 +0,0 @@
-import React, { HTMLAttributes } from "react";
-
-type EmptyProps = HTMLAttributes<HTMLDivElement>;
-export const Empty = ({ ...props }: EmptyProps) => {
-  return <div {...props}>테스트</div>;
-};
