c6d57013476e33dc3ac4d06ba2244c6764687075
장호빈 <devhobin@gmail.com>
2023-02-23 03:45:59 +0900

    styled components 삭제 및 stitch 추가

--- packages/primitives/.storybook/preview.js
+++ packages/primitives/.storybook/preview.js
@@ -1,16 +1,3 @@
-import { createGlobalStyle } from "styled-components";
-
-const GlobalStyle = createGlobalStyle``;
-
-const withGlobalStyle = (Story) => (
-  <>
-    <GlobalStyle />
-    <Story />
-  </>
-);
-
 export const parameters = {
   layout: "fullscreen",
 };
-
-export const decorators = [withGlobalStyle];
